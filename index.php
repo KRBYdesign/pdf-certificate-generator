ffb913a801db6a832c1e47a30e1a006c91bdd23b
Kyle Beasley <kyle.beasley17@gmail.com>
2025-01-22 12:34:59 -0600

    config form styling

--- index.php
+++ index.php
@@ -30,23 +30,19 @@ try {
     <body>
         <?php if (isset($header)) { echo $header; } ?>
 
-        <form action='./process.php' method="post">
-            <div class='input-container'>
-                <label for='file-prefix'>File Prefix
-                    <input type='text' name='file-prefix' required />
-                </label>
-            </div>
+        <form action='./process.php' method="post" id="configuration-form">
 
-            <div class='input-container'>
-                <label for='pdf-template'>Certificate Template
-                    <select name="pdf-template">
-                        <option value='default' disabled selected>Select A Template</option>
-                        <option value='premortem-training'>Premortem Training</option>
-                        <!-- <option value=''></option>
-                        <option value=''></option> -->
-                    </select>
-                </label>
-            </div>
+            <label for='file-prefix' class="input-container">File Prefix
+                <input type='text' name='file-prefix' required />
+            </label>
+
+            <label for='pdf-template' class="input-container">Certificate Template
+                <select name="pdf-template">
+                    <option value='default' disabled selected>Select A Template</option>
+                    <option value='premortem-training'>Premortem Training</option>
+                </select>
+            </label>
+
         </form> 
 
         <script src="index.js"></script>
