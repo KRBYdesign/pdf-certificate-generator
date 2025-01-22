b1ef4bfc150c40c2b297a99c9bbeaef91b1473e3
Kyle Beasley <kyle.beasley17@gmail.com>
2025-01-22 12:26:52 -0600

    more styling

--- includes/header.php
+++ includes/header.php
@@ -1,9 +1,9 @@
 <header>
-    <div class="header">
+    <div class="header title">
         <h1 class="text-logo">PDF Generator</h1>
 
         <nav class="menu">
-            <a href="./index.php">Home</a>
+            <a href="./index.php" class="copy link">Home</a>
         </nav>
     </div>
 </header>
